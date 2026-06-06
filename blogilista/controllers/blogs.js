8cdf7520eee9c1033e1c597b68e61352e7044311
heespari <heta.e.parikka@student.jyu.fi>
2026-06-06 16:26:35 +0300

    4.15: käyttäjän lisääminen tietokantaan

--- blogilista/controllers/blogs.js
+++ blogilista/controllers/blogs.js
@@ -1,6 +1,7 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 const User = require('../models/user')
+const jwt = require('jsonwebtoken')
 
 //refaktorointi: async/await, virheenkäsittely, 404-virhekoodi, id:n tarkistus
 blogsRouter.get('/', async (request, response) => {
@@ -17,9 +18,22 @@ blogsRouter.get('/:id', async (request, response) => {
   }
 })
 
+// refaktorointi: async/await, virheenkäsittely, 400-virhekoodi, id:n tarkistus
+const getTokenFrom = request => {
+  const authorization = request.get('authorization')
+  if (authorization && authorization.startsWith('Bearer ')) {
+    return authorization.replace('Bearer ', '')
+  }
+  return null
+}
+
 blogsRouter.post('/', async(request, response) => {
   const body = request.body
-  const user = await User.findById(body.userId)
+  const decodedToken = jwt.verify(getTokenFrom(request), process.env.SECRET)
+  if (!decodedToken.id) {
+    return response.status(401).json({ error: 'token invalid' })
+  }
+  const user = await User.findById(decodedToken.id)
 
   if (!user) {
     return response.status(400).json({ error: 'userId missing or not valid' })
